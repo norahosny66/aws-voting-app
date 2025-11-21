915ce9b6bc3bf9540589a8a1c5cbe2ac2edc8043
noura.hosny <noura.hosny@GizaSystems.onmicrosoft.com>
2025-11-21 23:54:12 +0200

    Save local changes before pull

--- infra/terraform/modules/iam/iam-github-actions.tf
+++ infra/terraform/modules/iam/iam-github-actions.tf
@@ -23,7 +23,7 @@ resource "aws_iam_role" "github_actions" {
             "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
           }
           StringLike = {
-            "token.actions.githubusercontent.com:sub" = "repo:norahosny66/aws-voting-app:ref:refs/heads/main"
+            "token.actions.githubusercontent.com:sub" = "repo:norahosny66/aws-voting-app:*"
             # Allow all branches (optional):
             # "token.actions.githubusercontent.com:sub" = "repo:YOUR_GITHUB_ORG/YOUR_REPO:*"
           }
